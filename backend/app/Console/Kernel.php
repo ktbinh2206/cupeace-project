213623465ccb1e41a549ab06c99b3a1ccb333b3a
ktbinh2206 <ktbinh2206@gmail.com>
2024-03-30 21:26:43 +0700

    refactor: :art: Comment schedule for handle streaming logs

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -18,19 +18,37 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
+            /**
+             * Get necessary data for calculate
+             * 1. Group song_id, user_id, streaming_session. Each session may have many small session
+             * 2. Order by created_at  descending
+             * 3. Get necessary data:
+             *  + if there are more than 2 small session, get all except last one (in case user is streaming)
+             *  + if there is only 1, get it (because user must finish  his/her stream)
+             */
             $data = DB::table('streaming_logs')
                 ->select(DB::raw('*, ROW_NUMBER() OVER (PARTITION BY user_id, streaming_session ORDER BY created_at DESC) AS row_num'))
                 ->orderBy('created_at', 'desc')
                 ->get()
                 ->groupBy(['user_id', 'streaming_session'])
                 ->map(function ($group, $user_id) {
-                    return [
-                        'user_id' => $user_id,
-                        'sessions' => $group->slice(1)->values()->all()
-                    ];
+                    if ($group->count() <= 1) {
+                        return [
+                            'user_id' => $user_id,
+                            'sessions' => $group->values()->all()
+                        ];
+                    } else {
+                        return [
+                            'user_id' => $user_id,
+                            'sessions' => $group->slice(1)->values()->all()
+                        ];
+                    }
                 })
                 ->values();
             $result = [];
+            /**
+             * Get the latest of data we got above
+             */
             foreach ($data as $datum) {
                 //sessions of each user
                 $sessions = $datum['sessions'];
@@ -40,6 +58,9 @@ class Kernel extends ConsoleKernel
                     array_push($result, $session[0]);
                 }
             }
+            /**
+             * Insert to user_song_action to count as a stream
+             */
             foreach ($result as $action) {
                 DB::table('user_song_actions')->insert([
                     'user_id' => $action->user_id,
@@ -50,6 +71,9 @@ class Kernel extends ConsoleKernel
                 ]);
             }
 
+            /**
+             * Delete all logs we have handled
+             */
             DB::table('streaming_logs')
                 ->select(DB::raw('*, ROW_NUMBER() OVER (PARTITION BY user_id, streaming_session ORDER BY created_at DESC) AS row_num'))
                 ->orderBy('created_at', 'desc')
--- backend/app/Http/Controllers/TestController.php
+++ backend/app/Http/Controllers/TestController.php
@@ -15,10 +15,19 @@ class TestController extends Controller
             ->get()
             ->groupBy(['user_id', 'streaming_session'])
             ->map(function ($group, $user_id) {
-                return [
-                    'user_id' => $user_id,
-                    'sessions' => $group->slice(1)->values()->all()
-                ];
+                if ($group->count() <= 1) {
+                    # code...
+
+                    return [
+                        'user_id' => $user_id,
+                        'sessions' => $group->values()->all()
+                    ];
+                } else {
+                    return [
+                        'user_id' => $user_id,
+                        'sessions' => $group->slice(1)->values()->all()
+                    ];
+                }
             })
             ->values();
         $result = [];
@@ -31,33 +40,34 @@ class TestController extends Controller
                 array_push($result, $session[0]);
             }
         }
-        foreach ($result as $action) {
 
-            DB::table('user_song_actions')->insert([
-                'user_id' => $action->user_id,
-                'song_id' => $action->song_id,
-                'duration' => $action->duration,
-                'created_at' => $action->created_at,
-                'action_type_id' => 2,
-            ]);
-        }
+        // foreach ($result as $action) {
 
-        DB::table('streaming_logs')
-            ->select(DB::raw('*, ROW_NUMBER() OVER (PARTITION BY user_id, streaming_session ORDER BY created_at DESC) AS row_num'))
-            ->orderBy('created_at', 'desc')
-            ->get()
-            ->groupBy(['user_id', 'streaming_session'])
-            ->each(function ($group) {
-                $group->slice(1)->values()->each(function ($sessions) {
-                    foreach ($sessions as $session) {
-                        DB::table('streaming_logs')
-                            ->where('user_id', $session->user_id)
-                            ->where('song_id', $session->song_id)
-                            ->where('streaming_session', $session->streaming_session)
-                            ->delete();
-                    }
-                });
-            });
+        //     DB::table('user_song_actions')->insert([
+        //         'user_id' => $action->user_id,
+        //         'song_id' => $action->song_id,
+        //         'duration' => $action->duration,
+        //         'created_at' => $action->created_at,
+        //         'action_type_id' => 2,
+        //     ]);
+        // }
+
+        // DB::table('streaming_logs')
+        //     ->select(DB::raw('*, ROW_NUMBER() OVER (PARTITION BY user_id, streaming_session ORDER BY created_at DESC) AS row_num'))
+        //     ->orderBy('created_at', 'desc')
+        //     ->get()
+        //     ->groupBy(['user_id', 'streaming_session'])
+        //     ->each(function ($group) {
+        //         $group->slice(1)->values()->each(function ($sessions) {
+        //             foreach ($sessions as $session) {
+        //                 DB::table('streaming_logs')
+        //                     ->where('user_id', $session->user_id)
+        //                     ->where('song_id', $session->song_id)
+        //                     ->where('streaming_session', $session->streaming_session)
+        //                     ->delete();
+        //             }
+        //         });
+        //     });
         return $result;
     }
 }
